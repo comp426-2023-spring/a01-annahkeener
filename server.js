f3193015febc7b419488de0b2a7a20cf07969ae9
Anna <ahk@live.unc.edu>
2023-02-10 17:33:19 -0500

    wowee did i do it?

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
 // Require http module
-const http = require('http');
+var http = require('http');
 
 // Require fs module
-const fs = require('fs');
+var fs = require('fs').promises;
 
 // Require minimist module (make sure you install this one via npm).
 const minimist = require('minimist');
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-var argv = require('minimist')(process.argv.slice(2));
-console.log(argv);
-
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 
-const port = 3000;
+const args = minimist(process.argv.slice(2));
+console.log(args);
+const port = (args.port || 3000);
+
+const host = 'localhost';
+
+
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-
-fs.readFile('./public/index.html', (err, data) => {
-	if (err) throw err;
-	console.log(data);
-});
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
@@ -38,23 +36,30 @@ fs.readFile('./public/index.html', (err, data) => {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('./public/index.html'), (err, data) => {
-	if (err) {
-		console.error(err)
-		return;
-	}
-}
-});
+let indexFile;
+
+const requestListener = function (req, res) {
+	res.setHeader("Content-Type", "text/html");
+	res.writeHead(200);
+	res.end(indexFile);
+};
+
+const server = http.createServer(requestListener);
+
+fs.readFile(__dirname + "/public/index.html")
+    .then(contents => {
+        indexFile = contents;
+        server.listen(port, host, () => {
+		console.log(`Server listening on port ${port}`);
+	 });
+    })
+    .catch(err => {
+        console.error(`Could not read index.html file: ${err}`);
+        process.exit(1);
+    });
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-server.listen(port, () => {
-	console.log('Server listening on port ${port}');
-});
-
 
 // That's it! You're all done!
